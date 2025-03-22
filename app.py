c1c822bf614c92861689815bd4ffccf80be07b47
Haomian-Ji <jokerjhm@gmail.com>
2025-03-22 15:39:14 +0000

    rename

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 import streamlit as st
+import streamlit_authenticator as stauth
 
-st.title("🎈 My new streamlit app")
+st.title("🎈 My new streamlit_authenticator app")
 st.write(
     "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
 )
