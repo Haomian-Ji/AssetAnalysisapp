b0b0c9a3d1d11f638cb57517be54df45e34a0cb8
Haomian-Ji <jokerjhm@gmail.com>
2025-03-25 15:25:00 +0000

    database

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,3 +5,9 @@ st.title("🎈 My new streamlit_authenticator app")
 st.write(
     "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
 )
+
+conn = st.connection("snowflake")
+df = conn.query("SELECT * FROM mytable;", ttl="10m")
+
+for row in df.itertuples():
+    st.write(f"{row.NAME} has a :{row.PET}:")
