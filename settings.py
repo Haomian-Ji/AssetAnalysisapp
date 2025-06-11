eb433954c48778e0c6a42cde24462cb3dea2f52c
Haomian-Ji <jokerjhm@gmail.com>
2025-06-11 07:13:14 +0000

    判空

--- settings.py
+++ settings.py
@@ -1,7 +1,12 @@
-import streamlit as st
-import streamlit_authenticator as stauth
+
 import yaml
 from yaml.loader import SafeLoader
 import os
 
-st.write("修改账户信息")
+import streamlit as st
+import streamlit_authenticator as stauth
+
+st.write("修改密码")
+password = st.text_input
+hashpassword = stauth.Hasher.hash(password)
+st.write(hashpassword)
